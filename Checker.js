7a6c7f87a4a19e1a41d26775fab0d9c667abeee7
Hitesh Kumar <nagpalhitesh521@gmail.com>
2024-09-17 22:13:59 +0530

    Update Checker.js

--- Checker.js
+++ Checker.js
@@ -1,6 +1,5 @@
-
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/TMC").then(()=>{
+mongoose.connect("mongodb+srv://hiteshnagpal:Hunnynagpal%402006@cluster0.jzero.mongodb.net/TMC?retryWrites=true&w=majority").then(()=>{
     console.log("db success")
 }).catch((e)=>{
     console.log(e)
